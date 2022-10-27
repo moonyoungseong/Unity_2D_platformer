989fd0d04086d6962e2ed5ab126165839c11ae16
moonyoungseong <ansdudtjd123@naver.com>
2022-10-27 23:13:14 +0900

    조사, UI 일부분 제작
    Raycast를 활용해 조사하기
    대화창에 상대 이름 띄우기 등

--- unity_2d_연습_탑다운RPG/Assets/Script/PlayerAction.cs
+++ unity_2d_연습_탑다운RPG/Assets/Script/PlayerAction.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class PlayerAction : MonoBehaviour
 {
+    public GameManager manager;
     float h;
     float v;
     Rigidbody2D rigid;
     bool isHorizonMove;
+    Vector3 dirVec;
+    GameObject scanObject;
     Animator anim;
 
     void Awake()
@@ -47,11 +50,36 @@ public class PlayerAction : MonoBehaviour
         }
         else
             anim.SetBool("isChange", false);
+
+        // Direction
+        if(vDown && v == 1)
+            dirVec = Vector3.up;
+        else if(vDown && v == -1)
+            dirVec = Vector3.down;
+        else if(hDown && h == -1)
+            dirVec = Vector3.left;
+        else if(hDown && h == 1)
+            dirVec = Vector3.right;
+
+        // Scan Object
+        if(Input.GetButtonDown("Jump") && scanObject != null)
+            manager.Action(scanObject);
     }
 
     void FixedUpdate() 
     {
+        // Move
         Vector2 moveVec = isHorizonMove ? new Vector2(h,0) : new Vector2(0,v);
         rigid.velocity = moveVec * 3;
+
+        // Ray
+        Debug.DrawRay(rigid.position, dirVec * 0.7f, new Color(0,1,0));
+        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, dirVec, 0.7f, LayerMask.GetMask("Object"));
+
+        if(rayHit.collider != null){
+            scanObject = rayHit.collider.gameObject;
+        }
+        else
+            scanObject = null;
     }
 }
